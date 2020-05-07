9a0e4f90739fd411aa47d2d2d22f45b5fd231f24
Wellington-BW <62914217+Wellington-BW@users.noreply.github.com>
2020-05-06 21:06:48 -0300

    Update ativ18.php

--- ativ18.php
+++ ativ18.php
@@ -7,6 +7,13 @@ if (preg_match($pattern, $a)) {
   echo 'Letra encontrada.';
 } else {
   echo 'Letra não encontrada.';
+} 
+echo '<br>';
+$bl = strpos($a,$b);
+
+if($bl===false){
+  echo 'Letra não encontrada na frase.';
+}else{
+  echo 'Letra encontrada na posição: '.$bl;
 }
-// b não feita.
 ?>
